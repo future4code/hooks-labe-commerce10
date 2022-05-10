1b1d1585893b149bb7eff63ae9b1e100ed4eae41
Ana Lívia <analivia.gdc@gmail.com>
2022-05-09 23:05:10 -0300

    Adicionado esqueleto do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppDad, Body, Footer, Header, SectionFiltros, SectionProdutos } from './styled';
+import { Carrinho } from './components/Carrinho';
+import GlobalStyle from './GlobalStyles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <AppDad>
+        <GlobalStyle />
+        <Header>
+          <div>
+            <img src="https://cdn-icons.flaticon.com/png/512/4710/premium/4710098.png?token=exp=1652145457~hmac=31ce7aa426610c9fc771aaeb92ca7bb6" alt="Imagem de um foquete subindo na diagonal"/>
+            <h2>Viagens Espaciais</h2>
+          </div>
+          <Carrinho></Carrinho>
+        </Header>
+        <Body>
+          <SectionFiltros> 
+            {/* Dentro dessa seção são os componentes de cada input*/}
+            {/* <InputValor></InputValor>
+            <InputValor></InputValor>
+            <inputBusca></inputBusca>
+            <InputOrdenar></InputOrdenar> */}
+          </SectionFiltros>
+          <h1>Produtos</h1>
+          <SectionProdutos>
+            {/* Dentro dessa seção vão os cards dos proodutos */}
+            {/* <CardProduto></CardProduto> */}
+          </SectionProdutos>
+        </Body>
+        <Footer>
+            <h2>Footer</h2>
+        </Footer>
+      </AppDad>
+    );
+  }
 }
 
 export default App;
