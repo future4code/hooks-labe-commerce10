792212ab46dde2d253f90aaebb4749efc3f7a419
Liz alves <elisabete_carente@hotmail.com>
2022-05-13 13:04:02 -0300

    funcionalidade de adicionar item ao carrinho

--- src/styled.js
+++ src/styled.js
@@ -30,13 +30,15 @@ export const Header = styled.div`
         max-height: 100%;
         width: auto;
     }
-    h2 {
-      text-shadow: 2px 2px 5px darkgrey;
-      position: absolute;
-      padding-left: 50px;
-      color: white;
-    };
 `
+export const TituloHeader = styled.h1`
+text-shadow: 2px 2px 5px darkgrey;
+position: absolute;
+padding-left: 50px;
+color: white;
+`
+//FIM HEARD
+
 
 //-----------ESTILIZAÇÕES DO BODY------------------
 export const Body = styled.div`
@@ -49,18 +51,18 @@ export const Body = styled.div`
     }
 `
 
-//CARRINHO
+//------------ ESTILIZAÇÃO DO CARRINHO
 export const Cart = styled.img`
-  height: 38px;
+  height: 35px;
   cursor: pointer;
   background-color: rgba(150, 163, 184, 0.4);
   color: black;
   border-radius: 50%;
   opacity: 1;
   position: absolute;
-  padding: 10px;
+  padding: 8px;
   right: 40px;
-  top: 12px;
+  top: 27px;
 `
 
 export const BotaoF = styled.button`
@@ -112,7 +114,7 @@ export const FinalizarCompra = styled.button`
 export const CarrinhoH2 = styled.h2`
   position: absolute;
   top: 24px;
-  left: -5px;
+  left: 25px;
   color: #ffffff;
   margin: 0;
   text-shadow: 2px 2px 5px darkgrey;
@@ -163,6 +165,91 @@ export const LiStyled = styled.li`
   border-radius: 4px;
 `
 //FIM CARRINHO
+
+
+//-----------ESTILIZAÇÕES DO CARDS DOS PRODUTOS------------------
+export const ContainerPrincipal = styled.div`
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+/* min-height: 100vh; */
+row-gap: 10px;
+margin-left: 20px;
+
+`;
+
+export const CardsProdutos = styled.div`
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  box-shadow: 2px 2px 5px darkgrey;
+  border-radius: 10px;
+  background-color: rgba(150, 163, 184, 0.4);
+  margin: 10px 7px;
+  color: rgb(95, 19, 39);
+  width: 18vw;
+  height: 53vh;
+`;
+
+export const TitleProduct = styled.h1`
+display: flex;
+align-items: center;
+justify-content: center;
+color: white;
+padding: 0.5em;
+`
+
+export const ImgSatelites = styled.img`
+width: 100%;
+height: 60%;
+border-radius: 10px 10px 0 0;
+`;
+
+export const InformacoesCard = styled.div`
+display: flex;
+flex-direction: column;
+align-content: center;
+align-items: center;
+color: white;
+margin: 15px;
+`;
+
+export const CardsTitulos = styled.h2`
+  color: #ffffff;
+  margin: 0;
+  text-shadow: 2px 2px 5px darkgrey;
+`;
+
+export const Preco = styled.p`
+  font-size: large;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+export const Button = styled.button`
+border-radius: 10px;
+width: 55%;
+padding: 8px;
+margin-top: 5px;
+border: none;
+background-color: #54295B;
+color: #ffffff;
+font-size: small;
+cursor: pointer;
+
+:hover {
+        padding: 10px;
+        margin: 2%;
+        max-width: 50%;
+        border-radius: 5px;
+        border: 1px solid #54295B;
+        color: #54295B;
+        font-size: 0.5em;
+        background-color: white;
+        align-self: center;
+    }
+`;
+
+// FIM CARDS PRODUTOS
 
 
 //POPUP
@@ -249,69 +336,68 @@ export const SectionProdutos = styled.div`
     max-width: 90vw;
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr ;
+    grid-template-columns: 1fr 1fr 1fr ;
     gap: 10px;
 `
 
 //CARDS
-export const DivCard = styled.div`
-    /* border: 1px solid red; */
-    max-height: fit-content;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-items: flex-start;
-    align-items: center;
-    margin-bottom: 15px;
-    justify-content: center;
-    box-shadow: 2px 2px 5px darkgrey;
-    border-radius: 10px;
-    background-color: rgba(150, 163, 184, 0.4);
-    margin: 10px 55px;
-    color: rgb(95, 19, 39);
-    width: 20vw;
-    height: 58vh;
+// export const DivCard = styled.div`
+//     /* border: 1px solid red; */
+//     max-height: fit-content;
+//     width: 100%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-items: flex-start;
+//     align-items: center;
+//     margin-bottom: 15px;
+//     justify-content: center;
+//     box-shadow: 2px 2px 5px darkgrey;
+//     border-radius: 10px;
+//     background-color: rgba(150, 163, 184, 0.4);
+//     margin: 10px 55px;
+//     color: rgb(95, 19, 39);
+//     width: 20vw;
+//     height: 58vh;
 
-    img {
-      width: 100%;
-      height: 72%;
-      border-radius: 10px 10px 0 0;
-    }
-    h3 {
-        color: white;
-        font-weight: bold;
-        margin: 3%;
-        text-align: center; 
-    }
-    p {
-        color: white;
-        margin: 0;
-    }
-    button {
-        padding: 10px;
-        margin: 2%;
-        max-width: 50%;
-        border-radius: 5px;
-        border: none;
-        color: white;
-        transition-duration: 0.4s;
-        font-size: 1em;
-        background-color: #54295B;
-        align-self: center;
-        cursor: pointer;
-    }
-    button:hover {
-        padding: 10px;
-        margin: 2%;
-        max-width: 50%;
-        border-radius: 5px;
-        border: 1px solid #54295B;
-        color: #54295B;
-        font-size: 1.2em;
-        background-color: white;
-        align-self: center;
-    }
-`
+//     img {
+//       width: 100%;
+//       height: 72%;
+//       border-radius: 10px 10px 0 0;
+//     }
+//     h3 {
+//         color: white;
+//         font-weight: bold;
+//         margin: 3%;
+//     }
+//     p {
+//         color: white;
+//         margin: 0;
+//     }
+//     button {
+//         padding: 10px;
+//         margin: 2%;
+//         max-width: 50%;
+//         border-radius: 5px;
+//         border: none;
+//         color: white;
+//         transition-duration: 0.4s;
+//         font-size: 1em;
+//         background-color: #54295B;
+//         align-self: center;
+//         cursor: pointer;
+//     }
+//     button:hover {
+//         padding: 10px;
+//         margin: 2%;
+//         max-width: 50%;
+//         border-radius: 5px;
+//         border: 1px solid #54295B;
+//         color: #54295B;
+//         font-size: 1.2em;
+//         background-color: white;
+//         align-self: center;
+//     }
+// `
 
 
 //-----------ESTILIZAÇÕES DO FOOTER
